5038206de8c7ef1a816a681c9151ed7123835ef8
obsluk00 <Luka.Obser@stud.unibas.ch>
2020-03-24 16:43:02 +0100

    cosmetical changes. previous commit made searchnodes more general.

--- UniformCostSearch.java
+++ UniformCostSearch.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 
 public class UniformCostSearch extends SearchAlgorithmBase {
 
-    private PriorityQueue<ElevatorSearchNode> openList;
+    private PriorityQueue<SearchNode> openList;
     private HashSet<State> closedList;
     private ArrayList<Action> optimalPath;
 
@@ -16,21 +16,21 @@ public class UniformCostSearch extends SearchAlgorithmBase {
     @Override
     protected ArrayList<Action> run() {
         ArrayList<Action> optimalPath = new ArrayList<Action>();
-        openList = new PriorityQueue<ElevatorSearchNode>(1, (a, b) -> a.pathCost - b.pathCost);
-        openList.add(new ElevatorSearchNode(stateSpace.init()));
+        openList = new PriorityQueue<SearchNode>(1, (a, b) -> a.pathCost - b.pathCost);
+        openList.add(new SearchNode(stateSpace.init()));
         closedList = new HashSet<State>();
         //for (ActionStatePair pair : stateSpace.succ(stateSpace.init())) {
         //    openList.add(new ElevatorSearchNode(stateSpace, new ElevatorSearchNode(), pair));
         //}
         while (!openList.isEmpty()) {
-            ElevatorSearchNode n = openList.remove();
+            SearchNode n = openList.remove();
             if (!closedList.contains(n.state)) {
                 closedList.add(n.state);
                 if (this.stateSpace.isGoal(n.state)) {
                     return n.extractPath(n);
                 }
                 for (ActionStatePair successor : this.stateSpace.succ(n.state)) {
-                    openList.add(new ElevatorSearchNode(n, successor, successor.action.cost()));
+                    openList.add(new SearchNode(n, successor, successor.action.cost()));
                 }
             }
         }
@@ -38,28 +38,27 @@ public class UniformCostSearch extends SearchAlgorithmBase {
         return new ArrayList<Action>();
     }
 
-
-    public class ElevatorSearchNode {
+    public class SearchNode {
         State state;
-        ElevatorSearchNode parent;
+        SearchNode parent;
         Action action;
         int pathCost;
 
-        public ElevatorSearchNode(State init){
+        public SearchNode(State init){
             this.state = init;
             this.parent = null;
             this.action = null;
             this.pathCost = 0;
         }
 
-        public ElevatorSearchNode(ElevatorSearchNode parent, ActionStatePair pair, int actioncost){
+        public SearchNode(SearchNode parent, ActionStatePair pair, int actioncost){
             this.state = pair.state;
             this.parent = parent;
             this.action = pair.action;
             this.pathCost = parent.pathCost + actioncost;
         }
 
-        public ArrayList<Action> extractPath(ElevatorSearchNode n) {
+        public ArrayList<Action> extractPath(SearchNode n) {
             ArrayList<Action> path = new ArrayList<Action>();
             while(n.parent != null){
                 path.add(n.action);
@@ -69,4 +68,5 @@ public class UniformCostSearch extends SearchAlgorithmBase {
             return path;
         }
     }
+
 }
